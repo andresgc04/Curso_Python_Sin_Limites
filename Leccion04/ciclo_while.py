3e5869a9695b41e9fd29840be0f0d6e21bd5d90b
andresgc04 <andresgc1997@outlook.com>
2023-06-07 14:45:54 -0400

    Creando Leccion 04 y Agregando Clase Sobre El Ciclo While

---
+++ Leccion04/ciclo_while.py
@@ -0,0 +1,17 @@
+"""
+condicion = True
+
+while condicion:
+    print('Ejecutando Ciclo While')
+else:
+    print('Fin del ciclo while')
+"""
+
+contador = 0
+
+while contador < 3:
+    print(contador)
+
+    contador +=1 #contador = contador + 1
+else:
+    print('Fin ciclo while')
