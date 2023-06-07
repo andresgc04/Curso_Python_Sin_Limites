6cd70deca7745afb9f330673c12b440820d51584
andresgc04 <andresgc1997@outlook.com>
2023-06-07 16:07:16 -0400

    Creando Leccion 05 y Agregando Clase Sobre Listas

---
+++ Leccion05/listas.py
@@ -0,0 +1,13 @@
+#Definir una lista de tipo str:
+nombres = ['Juan', 'Karla', 'Ricardo', 'María']
+
+#Imprimir la lista de nombres:
+print(nombres)
+
+#Acceder a los elementos de una lista:
+print(nombres[0])
+print(nombres[1])
+
+#Acceder a los elementos de manera inversa:
+print(nombres[-1])
+print(nombres[-2])
