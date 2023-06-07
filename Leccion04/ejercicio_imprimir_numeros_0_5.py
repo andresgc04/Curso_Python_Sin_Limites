3704bd1b6ef5b776968ce8b3c098f37212ca7ff9
andresgc04 <andresgc1997@outlook.com>
2023-06-07 14:58:38 -0400

    Agregando Ejercicio De Imprimir Numeros Del 0 Al 5 Utilizando El Bucle While.

---
+++ Leccion04/ejercicio_imprimir_numeros_0_5.py
@@ -0,0 +1,11 @@
+#Imprimir los números enteros del 0 al 5 utilizando un ciclo while:
+
+maximo = 5
+contador = 0
+
+while contador <= maximo:
+    print(contador)
+
+    contador += 1
+else:
+    print('Fin del ciclo while')
